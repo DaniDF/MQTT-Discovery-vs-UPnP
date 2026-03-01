a41b04a3b26b2015142a4faf40a61723101a9637
daniDF <danieledraghi2@gmail.com>
2026-03-01 12:05:38 +0000

    Updated gena tests

--- main-control/main.go
+++ main-control/main.go
@@ -100,6 +100,13 @@ func main() {
 		}
 	}
 
+	//testSoap(ctx, args, mx, logLevel)
+	testGena(ctx, args, mx, logLevel)
+}
+
+func testSoap(ctx context.Context, args Args, mx int, logLevel slog.Level) {
+	log := ctx.Value("logger").(logging.Logger)
+
 	waitUpnpControls := make(chan bool, args.NumUpnpControl)
 	for range args.NumUpnpControl {
 		go func() {
@@ -138,27 +145,25 @@ func main() {
 
 					var startRPCTime time.Time
 
-					//waitGena := make(chan bool, 1)
+					waitGena := make(chan bool, 1)
 
 					// Start - GENA
-					//var cancel context.CancelFunc
-					/*
-						_, err := upnp.Subscribe(ctx, rootDevice, testService, func(event string) {
-							log.Trace(i + " [main-control] Event elapsed time: " + time.Since(startRPCTime).String())
-							log.Debug("[main-control] Received event: " + event)
+					var cancel context.CancelFunc
+					cancelP, err := upnp.Subscribe(ctx, rootDevice, testService, func(event string) {
+						log.Trace("[main-control] Event elapsed time: " + time.Since(startRPCTime).String())
+						log.Debug("[main-control] Received event: " + event)
 
-							//cancel()
-							upnp.Unsubscribe(ctx, rootDevice, testService)
-							waitGena <- true
+						cancel()
+						upnp.Unsubscribe(ctx, rootDevice, testService)
+						waitGena <- true
 
-						})
-						if err != nil {
-							log.Error("[main-control] Error subscribing to " + testService.ServiceId + ", " + err.Error())
-							waitRootDevice <- true
-							return
-						}
-					*/
-					//cancel = *cancelP
+					})
+					if err != nil {
+						log.Error("[main-control] Error subscribing to " + testService.ServiceId + ", " + err.Error())
+						waitRootDevice <- true
+						return
+					}
+					cancel = *cancelP
 					// End - GENA
 
 					// Start - SOAP
@@ -179,13 +184,11 @@ func main() {
 					log.Trace("[main-control] RPC Elapsed time: " + elapsedTime.String())
 					// End - SOAP
 
-					/*
-						select {
-						case <-waitGena:
-						case <-time.After(genaTimeout):
-							log.Warn("[main-control] Not received gena response before timeout")
-						}
-					*/
+					select {
+					case <-waitGena:
+					case <-time.After(genaTimeout):
+						log.Warn("[main-control] Not received gena response before timeout")
+					}
 
 					waitRootDevice <- true
 				}()
@@ -205,6 +208,123 @@ func main() {
 		case <-time.After(upnpControlsTimeout + time.Duration(mx)*time.Second):
 			log.Warn("[main-control] Not all the upnp controls have returned before timeout")
 			return
+		}
+	}
+}
+
+func testGena(ctx context.Context, args Args, mx int, logLevel slog.Level) {
+	log := ctx.Value("logger").(logging.Logger)
+
+	// Start - SSDP
+	startSearchTime := time.Now()
+	rootDevices, err := upnp.SearchMx(ctx, "urn:schemas-upnp-org:device:BinaryLight:1", mx)
+	if err != nil {
+		log.Error("[main-control] Error fetching rootDevices: " + err.Error())
+		return
+	}
+	elapsedTime := time.Since(startSearchTime)
+	log.Info("[main-control] Found " + strconv.Itoa(len(rootDevices)) + " devices")
+	log.Trace("[main-control] SEARCH Elapsed time: " + elapsedTime.String())
+
+	if logLevel <= slog.LevelDebug {
+		i := 0
+		for _, rootDevice := range rootDevices {
+			fmt.Println(strconv.Itoa(i) + ") " + upnp.StringRootDevice(rootDevice))
+			i++
+		}
+	}
+	// End - SSDP
+
+	if len(rootDevices) > 0 {
+		udns := []string{}
+		for key := range rootDevices {
+			udns = append(udns, key)
+		}
+		rootDevice := rootDevices[udns[0]]
+		testService := rootDevice.Device.Services[0]
+
+		type TurnArgs struct {
+			StateValue string `xml:"StateValue"`
+		}
+		type TurnReply struct {
+			ActualValue string `xml:"ActualValue"`
+		}
+
+		var startRPCTime time.Time
+
+		waitGenaSubscriptions := make(chan bool, args.NumUpnpControl)
+		waitUpnpControls := make(chan bool, args.NumUpnpControl)
+		for range args.NumUpnpControl {
+			go func() {
+				testService := rootDevice.Device.Services[0]
+
+				waitGena := make(chan bool, 1)
+
+				// Start - GENA
+				var cancel context.CancelFunc
+				cancelP, err := upnp.Subscribe(ctx, rootDevice, testService, func(event string) {
+					log.Trace("[main-control] Event elapsed time: " + time.Since(startRPCTime).String())
+					log.Debug("[main-control] Received event: " + event)
+
+					cancel()
+					upnp.Unsubscribe(ctx, rootDevice, testService)
+					waitGena <- true
+
+				})
+				if err != nil {
+					log.Error("[main-control] Error subscribing to " + testService.ServiceId + ", " + err.Error())
+					waitUpnpControls <- true
+					waitGenaSubscriptions <- true
+					return
+				}
+				cancel = *cancelP
+				// End - GENA
+
+				waitGenaSubscriptions <- true
+
+				select {
+				case <-waitGena:
+				case <-time.After(genaTimeout):
+					log.Warn("[main-control] Not received gena response before timeout")
+				}
+
+				waitUpnpControls <- true
+			}()
+		}
+
+		for range args.NumUpnpControl {
+			select {
+			case <-waitGenaSubscriptions:
+			case <-time.After(genaTimeout):
+				log.Warn("[main-control] Not all the upnp controls have successful subscribed before timeout")
+			}
+		}
+
+		// Start - SOAP
+		soap := testService.NewSOAPClient()
+		soapArgs := TurnArgs{
+			StateValue: "1",
+		}
+		reply := TurnReply{}
+
+		startRPCTime = time.Now()
+		err = soap.PerformActionCtx(ctx, testService.ServiceType, "Turn", &soapArgs, &reply)
+		if err != nil {
+			log.Error("[main-control] Error RPC: " + err.Error())
+		}
+		elapsedTime = time.Since(startRPCTime)
+
+		log.Info("[main-control] RPC returned: " + reply.ActualValue)
+		log.Trace("[main-control] RPC Elapsed time: " + elapsedTime.String())
+		// End - SOAP
+
+		for range args.NumUpnpControl {
+			select {
+			case <-waitUpnpControls:
+			case <-time.After(upnpControlsTimeout + time.Duration(mx)*time.Second):
+				log.Warn("[main-control] Not all the upnp controls have returned before timeout")
+				return
+			}
 		}
 	}
 }
--- tests/scripts/upnp_latency_control.sh
+++ tests/scripts/upnp_latency_control.sh
@@ -6,10 +6,12 @@ trap ctrl_c INT
 
 function ctrl_c() {
     echo STOPPING...
+    ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
     ssh -S /tmp/ssh-device -O exit user@10.2.0.107 > /dev/null
     exit
 }
 
+### Soap test
 for upnp_devices in {1..9} {10..100..10} ;
 do
     ssh -f -MS /tmp/ssh-device user@10.2.0.107 "tests/bin/device -u $upnp_devices > tests/logs/device_test-latency_u-"$upnp_devices"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
@@ -25,6 +27,26 @@ do
             sleep 2
             ../bin/./control -u $upnp_controls > ../logs/control_test-latency_d-"$upnp_devices"_u-"$upnp_controls"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null
         done
+    done
+
+    ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
+    ssh -S /tmp/ssh-device -O exit user@10.2.0.107 > /dev/null 2>&1
+done
+
+### Gena test
+
+for upnp_controls in {1..9} {10..100..10} ;
+do
+    ssh -f -MS /tmp/ssh-device user@10.2.0.107 "tests/bin/device -u 1 > tests/logs/device_test-gena_u-1_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
+
+    sleep 3
+
+    echo "Running upnp_controls $upnp_controls"
+    
+    for count in {1..30} ;
+    do
+        sleep 2
+        ../bin/./control -u $upnp_controls > ../logs/control_test-gena_d-1_u-"$upnp_controls"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null
     done
 
     ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
