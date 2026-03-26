2cfed9f50fe048960db577176ce0598faa5e6436
daniDF <danieledraghi2@gmail.com>
2026-03-26 22:44:55 +0000

    Fixed pkill bug

--- tests/scripts/mqtt_latency_control.sh
+++ tests/scripts/mqtt_latency_control.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+REMOTE_ROOT='root@10.2.0.107'
+REMOTE_USER='user@10.2.0.107'
+
 ## Test latency
 
 trap ctrl_c INT
 
+function kill_remote_jobs() {
+    for pid in $(ssh $REMOTE_ROOT 'ps aux | grep -E "[0-9]+ (bash -c )?tests/bin/device" | awk '\''{$1=$1}1'\'' | cut -d" " -f2') ;
+    do
+        ssh $REMOTE_ROOT 'kill -9 '$pid
+    done
+}
+
 function ctrl_c() {
     echo STOPPING...
-    ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
-    ssh -S /tmp/ssh-device -O exit user@10.2.0.107 > /dev/null
+    kill_remote_jobs
+    ssh -O exit $REMOTE_USER > /dev/null
     exit
 }
 
@@ -15,22 +25,21 @@ for mqtt_devices in {1..9} {10..100..10} ;
 do
     for qos in {0..2} ;
     do
-        ssh -f -MS /tmp/ssh-device user@10.2.0.107 "tests/bin/device -m $mqtt_devices --mqtt-broker 10.2.0.108:1883 --qos "$qos" > tests/logs/device_test-latency_m-"$mqtt_devices"_q-"$qos"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
-
-        sleep 5
-
-        for mqtt_controls in {1..9} {10..100..10}
+        for mqtt_controls in {1..9} {10..100..10} ;
         do
             echo "Running mqtt_devices: $mqtt_devices, mqtt_controls $mqtt_controls, qos: $qos"
-            
+
             for count in {1..30} ;
             do
-                sleep 2
+                ssh -f $REMOTE_USER "tests/bin/device -m $mqtt_devices --mqtt-broker 10.2.0.108:1883 --qos "$qos" > tests/logs/device_test-latency_m-"$mqtt_devices"_q-"$qos"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
+                
+                sleep 4
+                
                 ../bin/./control -m "$mqtt_controls" --mqtt-broker 10.2.0.108:1883 --qos "$qos" > ../logs/control_test-latency_d-"$mqtt_devices"_m-"$mqtt_controls"_q-"$qos"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null
+
+                kill_remote_jobs
+                sleep 1
             done
         done
-
-        ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
-        ssh -S /tmp/ssh-device -O exit user@10.2.0.107
     done
 done
--- tests/scripts/upnp_latency_control.sh
+++ tests/scripts/upnp_latency_control.sh
@@ -1,54 +1,61 @@
 #!/bin/bash
 
+REMOTE_ROOT='root@10.2.0.107'
+REMOTE_USER='user@10.2.0.107'
+
 ## Test latency
 
 trap ctrl_c INT
 
+function kill_remote_jobs() {
+    for pid in $(ssh $REMOTE_ROOT 'ps aux | grep -E "[0-9]+ (bash -c )?tests/bin/device" | awk '\''{$1=$1}1'\'' | cut -d" " -f2') ;
+    do
+        ssh $REMOTE_ROOT 'kill -9 '$pid
+    done
+}
+
 function ctrl_c() {
     echo STOPPING...
-    ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
-    ssh -S /tmp/ssh-device -O exit user@10.2.0.107 > /dev/null
+    kill_remote_jobs
+    ssh -O exit $REMOTE_USER > /dev/null
     exit
 }
 
 ### Soap test
 for upnp_devices in {1..9} {10..100..10} ;
 do
-    ssh -f -MS /tmp/ssh-device user@10.2.0.107 "tests/bin/device -u $upnp_devices > tests/logs/device_test-latency_u-"$upnp_devices"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
-
-    sleep 5
-
     for upnp_controls in {1..9} {10..100..10} ;
     do
         echo "Running upnp_devices: $upnp_devices, upnp_controls $upnp_controls"
-        
+
         for count in {1..30} ;
         do
-            sleep 2
+            ssh -f $REMOTE_USER "tests/bin/device -u $upnp_devices > tests/logs/device_test-latency_u-"$upnp_devices"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
+            
+            sleep 3
+
             ../bin/./control -u $upnp_controls > ../logs/control_test-latency_d-"$upnp_devices"_u-"$upnp_controls"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null
+        
+            kill_remote_jobs
+            sleep 1
         done
     done
-
-    ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
-    ssh -S /tmp/ssh-device -O exit user@10.2.0.107 > /dev/null 2>&1
 done
 
 ### Gena test
 
 for upnp_controls in {1..9} {10..100..10} ;
 do
-    ssh -f -MS /tmp/ssh-device user@10.2.0.107 "tests/bin/device -u 1 > tests/logs/device_test-gena_u-1_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
-
-    sleep 3
-
     echo "Running upnp_controls $upnp_controls"
     
     for count in {1..30} ;
     do
-        sleep 2
+        ssh $REMOTE_USER "tests/bin/device -u 1 > tests/logs/device_test-gena_u-1_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null"
+        
+        sleep 3
         ../bin/./control -u $upnp_controls > ../logs/control_test-gena_d-1_u-"$upnp_controls"_$(date +"%Y%m%d_%H%M%S_%N").log 2>&1 < /dev/null
-    done
 
-    ssh root@10.2.0.107 'pkill -f "tests/bin/device -u"' > /dev/null
-    ssh -S /tmp/ssh-device -O exit user@10.2.0.107 > /dev/null 2>&1
+        kill_remote_jobs
+        sleep 1
+    done
 done
